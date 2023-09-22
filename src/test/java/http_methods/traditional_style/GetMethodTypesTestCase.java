519472d0537012d702f5a382055956b6d37a7558
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-09-22 08:10:00 +0530

    Different Types of Get Method with Description

--- src/test/java/http_methods/traditional_style/GetMethodTypesTestCase.java
+++ src/test/java/http_methods/traditional_style/GetMethodTypesTestCase.java
@@ -9,6 +9,12 @@ import org.testng.annotations.Test;
 
 public class GetMethodTypesTestCase {
 
+    /*
+       1. The below method directly sends a GET request to the specified URL without the need for
+          creating a separate 'RequestSpecification' object.
+       2. It's a simple way to send a GET request if you don't need to set custom headers or
+          parameters for that specific request.
+    */
     @Test(priority = 1)
     public void getMethodType1(){
 
@@ -22,6 +28,14 @@ public class GetMethodTypesTestCase {
         System.out.println("Response status code is "+statusCode);
     }
 
+    /*
+        1. The below method make use of 'RequestSpecification' object named 'requestSpecification'
+           using RestAssured.
+        2. This method uses the 'request()' method of the 'RequestSpecification' object to explicitly
+           specify the HTTP method (GET, in this case) and send the request.
+        3. This approach is useful when you want to reuse the same 'RequestSpecification' for multiple
+           requests and customize headers, parameters, or other request details.
+    */
     @Test(priority = 2)
     public void getMethodType2(){
 
@@ -37,6 +51,14 @@ public class GetMethodTypesTestCase {
         System.out.println("Response status code is "+statusCode);
     }
 
+    /*
+        1. Similar to the previous method, the below method also make use of 'RequestSpecification'
+           object named 'requestSpecification' using RestAssured.
+        2. In this case, the 'get()' method directly being used on the 'RequestSpecification' object
+           to send a GET request.
+        3. It's a more concise way to send a GET request using the 'RequestSpecification' you have
+           already configured.
+    */
     @Test(priority = 3)
     public void getMethodType3(){
 
