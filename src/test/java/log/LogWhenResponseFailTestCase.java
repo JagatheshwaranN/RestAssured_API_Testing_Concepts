dcb8dd091ea2e191c6f63b725439e135325e7f5b
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-09-23 07:49:32 +0530

    Log Scenario and Assertion Scenario added

---
+++ src/test/java/log/LogWhenResponseFailTestCase.java
@@ -0,0 +1,25 @@
+package log;
+
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+/**
+ * @restapi
+ * @get
+ */
+public class LogWhenResponseFailTestCase {
+
+    @Test(priority = 1)
+    public void generateLogWhenValidationFail(){
+        given()
+        .when()
+                .log().ifValidationFails()
+                .get("https://reqres.in/api/users/2")
+        .then()
+                .statusCode(200)
+                .body("data.id", equalTo(2));
+    }
+
+}
--- src/test/java/response_validation/ResponseValidationApproach1TestCase.java
+++ src/test/java/response_validation/ResponseValidationApproach1TestCase.java
@@ -9,13 +9,26 @@ import static io.restassured.RestAssured.*;
 public class ResponseValidationApproach1TestCase {
 
     @Test(priority = 1)
-    public void validateResponseApproach1() {
+    public void validateResponseApproach1Type1() {
 
         given()
         .when()
                 .get("http://localhost:3000/store")
         .then()
                 .statusCode(200)
+                .body("books[1].title", equalTo("The Art of Cooking"));
+    }
+
+    @Test(priority = 2)
+    public void validateResponseApproach1Type2() {
+
+        given()
+        .when()
+                .get("http://localhost:3000/store")
+        .then()
+                .assertThat()
+                .statusCode(200)
+                .and()
                 .body("books[1].title", equalTo("The Art of Cooking"));
     }
 }
