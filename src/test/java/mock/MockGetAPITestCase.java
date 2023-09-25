fc37ec94e0d093fbd785358b7f70cc8297d03e84
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-09-25 08:40:32 +0530

    Mock API Response

---
+++ src/test/java/mock/MockGetAPITestCase.java
@@ -0,0 +1,28 @@
+package mock;
+
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+/**
+ * @restapi
+ * @get
+ * @mock
+ */
+public class MockGetAPITestCase {
+
+    @Test(priority = 1)
+    public void mockGetApiTest(){
+
+        given()
+                .log()
+                .all()
+        .when()
+                .get("https://run.mocky.io/v3/7b474ce4-6d06-4255-a08d-38ba9544588a")
+        .then()
+                .statusCode(200)
+                .log()
+                .all();
+    }
+
+}
