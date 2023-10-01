444f4c843debbc9b6ee40818f434f7ffcec7cb56
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-10-01 11:08:05 +0530

    Response - Verify Type of Responses

---
+++ src/test/java/response_validation/restassured/VerifyTypeOfResponseTestCase.java
@@ -0,0 +1,43 @@
+package response_validation.restassured;
+
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @restapi
+ * @get
+ */
+public class VerifyTypeOfResponseTestCase {
+
+    @Test(priority = 1)
+    public void validateObjectResponse(){
+
+        RestAssured.given()
+                .get("http://localhost:3000/employees/1")
+                .then()
+                .body("", Matchers.instanceOf(Map.class));
+    }
+
+    @Test(priority = 2)
+    public void validateArrayResponse(){
+
+        RestAssured.given()
+                .get("http://localhost:3000/addresses")
+                .then()
+                .body("", Matchers.instanceOf(List.class));
+    }
+
+    @Test(priority = 3)
+    public void validatePartialResponse(){
+
+        RestAssured.given()
+                .get("http://localhost:3000/students/1")
+                .then()
+                .body("courses", Matchers.instanceOf(List.class));
+    }
+
+}
