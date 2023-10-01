a5a3fe318146e5deaca805dabfdc3298777b2876
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-10-01 11:20:53 +0530

    Added Lombok dependency and updated the POJO for Getter 7 Setter usage

--- src/main/java/pojo/Book.java
+++ src/main/java/pojo/Book.java
@@ -1,7 +1,11 @@
 package pojo;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class Book {
 
     @JsonProperty("book name")
@@ -9,21 +13,5 @@ public class Book {
 
     @JsonProperty("book author")
     private String bookAuthor;
-
-    public String getBookName() {
-        return bookName;
-    }
-
-    public void setBookName(String bookName) {
-        this.bookName = bookName;
-    }
-
-    public String getBookAuthor() {
-        return bookAuthor;
-    }
-
-    public void setBookAuthor(String bookAuthor) {
-        this.bookAuthor = bookAuthor;
-    }
 
 }
