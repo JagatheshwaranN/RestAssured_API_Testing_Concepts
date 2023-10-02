eb3cb9ed1e618739458c18b8abd833e7ee514489
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-10-02 10:12:26 +0530

    Json Schema Validation Scenarios Updated

--- src/test/java/schema_validation/JsonSchemaValidationTestCase.java
+++ src/test/java/schema_validation/JsonSchemaValidationTestCase.java
@@ -3,6 +3,10 @@ package schema_validation;
 import io.restassured.module.jsv.JsonSchemaValidator;
 import org.testng.annotations.Test;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 import static io.restassured.RestAssured.given;
 
 /**
@@ -12,7 +16,7 @@ import static io.restassured.RestAssured.given;
 public class JsonSchemaValidationTestCase {
 
     @Test(priority = 1)
-    public void validateJsonSchema() {
+    public void validateJsonSchemaType1() {
 
         given()
         .when()
@@ -21,4 +25,31 @@ public class JsonSchemaValidationTestCase {
                 .statusCode(200)
                 .assertThat().body(JsonSchemaValidator.matchesJsonSchemaInClasspath("json_schema.json"));
     }
+
+    @Test(priority = 2)
+    public void validateJsonSchemaType2() {
+
+        File jsonSchema = new File("src//test//resources//json_schema.json");
+
+        given()
+        .when()
+                .get("http://localhost:3000/students/1")
+        .then()
+                .statusCode(200)
+                .assertThat().body(JsonSchemaValidator.matchesJsonSchema(jsonSchema));
+    }
+
+    @Test(priority = 3)
+    public void validateJsonSchemaType3() throws FileNotFoundException {
+
+        FileInputStream jsonSchema = new FileInputStream("src//test//resources//json_schema.json");
+
+        given()
+        .when()
+                .get("http://localhost:3000/students/1")
+        .then()
+                .statusCode(200)
+                .assertThat().body(JsonSchemaValidator.matchesJsonSchema(jsonSchema));
+    }
+
 }
