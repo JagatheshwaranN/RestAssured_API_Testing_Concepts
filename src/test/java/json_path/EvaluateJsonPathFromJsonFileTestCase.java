7f8e3ef06f494e100cf0356f00d1317eacd2fe38
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-10-04 16:46:06 +0530

    JsonPath Evaluation Using Different Approach

--- src/test/java/json_path/EvaluateJsonPathFromJsonFileTestCase.java
+++ src/test/java/json_path/EvaluateJsonPathFromJsonFileTestCase.java
@@ -1,6 +1,7 @@
 package json_path;
 
 import com.jayway.jsonpath.Configuration;
+import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
 import org.testng.annotations.Test;
 
@@ -12,13 +13,46 @@ import java.util.List;
 public class EvaluateJsonPathFromJsonFileTestCase {
 
     @Test(priority = 1)
-    public void evaluateJsonPathFromJson() throws IOException {
+    public void evaluateJsonPathUsingNormalApproach() throws IOException {
+
+        try(InputStream jsonFile = new FileInputStream("src//test//resources//book_store.json")){
+
+            List<Object> prices = JsonPath.read(jsonFile, "$..price");
+            prices.forEach(System.out::println);
+        }
+    }
+
+    @Test(priority = 2)
+    public void evaluateJsonPathUsingConfigurationApproach() throws IOException {
 
         try(InputStream jsonFile = new FileInputStream("src//test//resources//book_store.json")){
 
             // The below line is used to parse the Json file ONLY once and read many times.
             Object parsedJsonFile = Configuration.defaultConfiguration().jsonProvider().parse(jsonFile.readAllBytes());
             List<Object> prices = JsonPath.read(parsedJsonFile, "$..price");
+            prices.forEach(System.out::println);
+        }
+    }
+
+    @Test(priority = 3)
+    public void evaluateJsonPathUsingDocumentContextApproach() throws IOException {
+
+        try(InputStream jsonFile = new FileInputStream("src//test//resources//book_store.json")){
+
+            DocumentContext context = JsonPath.parse(jsonFile);
+            List<Object> prices = context.read("$..price");
+            prices.forEach(System.out::println);
+        }
+    }
+
+    @Test(priority = 4)
+    public void evaluateJsonPathUsingDocumentContextAndConfigApproach() throws IOException {
+
+        try(InputStream jsonFile = new FileInputStream("src//test//resources//book_store.json")){
+
+            Configuration configuration = Configuration.defaultConfiguration();
+            DocumentContext context = JsonPath.using(configuration).parse(jsonFile);
+            List<Object> prices = context.read("$..price");
             prices.forEach(System.out::println);
         }
     }
