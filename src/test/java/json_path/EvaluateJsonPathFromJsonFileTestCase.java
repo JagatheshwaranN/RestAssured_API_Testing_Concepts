0602172fafeada229f7f2a25567f84a1f1ca66bc
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-10-04 08:57:48 +0530

    JsonPath Evaluation Using Jay-way Inprogress

--- src/test/java/json_path/EvaluateJsonPathFromJsonFileTestCase.java
+++ src/test/java/json_path/EvaluateJsonPathFromJsonFileTestCase.java
@@ -1,10 +1,12 @@
 package json_path;
 
+import com.jayway.jsonpath.Configuration;
 import com.jayway.jsonpath.JsonPath;
 import org.testng.annotations.Test;
 
-import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 public class EvaluateJsonPathFromJsonFileTestCase {
@@ -12,8 +14,12 @@ public class EvaluateJsonPathFromJsonFileTestCase {
     @Test(priority = 1)
     public void evaluateJsonPathFromJson() throws IOException {
 
-        File jsonFile = new File("src//test//resources//book_store.json");
-        List<Object> prices = JsonPath.read(jsonFile, "$..price");
-        prices.forEach(System.out::println);
+        try(InputStream jsonFile = new FileInputStream("src//test//resources//book_store.json")){
+
+            // The below line is used to parse the Json file ONLY once and read many times.
+            Object parsedJsonFile = Configuration.defaultConfiguration().jsonProvider().parse(jsonFile.readAllBytes());
+            List<Object> prices = JsonPath.read(parsedJsonFile, "$..price");
+            prices.forEach(System.out::println);
+        }
     }
 }
