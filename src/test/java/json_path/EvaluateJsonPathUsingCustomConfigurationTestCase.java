21711f82aeee095c45cf5aa03268b59535461184
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-10-06 07:39:44 +0530

    JsonPath Evaluation Using Custom Configuration Updated

--- src/test/java/json_path/EvaluateJsonPathUsingCustomConfigurationTestCase.java
+++ src/test/java/json_path/EvaluateJsonPathUsingCustomConfigurationTestCase.java
@@ -5,12 +5,11 @@ import com.jayway.jsonpath.JsonPath;
 import com.jayway.jsonpath.Option;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class EvaluateJsonPathUsingCustomConfigurationTestCase {
 
-    @Test(priority = 1)
-    public void evaluateJsonPathUsingCustomConfigurationApproach1() {
-
-        String sampleJson = """
+    public String sampleJson = """
                 [
                   {
                     "name": "John",
@@ -20,6 +19,10 @@ public class EvaluateJsonPathUsingCustomConfigurationTestCase {
                     "name": "Alex"
                   }
                 ]""";
+
+    @Test(priority = 1)
+    public void evaluateJsonPathUsingCustomConfigurationApproach1() {
+
         Configuration configuration = Configuration.defaultConfiguration();
         configuration.addOptions(Option.DEFAULT_PATH_LEAF_TO_NULL);
 
@@ -29,17 +32,6 @@ public class EvaluateJsonPathUsingCustomConfigurationTestCase {
 
     @Test(priority = 2)
     public void evaluateJsonPathUsingCustomConfigurationApproach2() {
-
-        String sampleJson = """
-                [
-                  {
-                    "name": "John",
-                    "mobile": 1234567890
-                  },
-                  {
-                    "name": "Alex"
-                  }
-                ]""";
 
         Configuration configuration =
                 Configuration
@@ -51,4 +43,45 @@ public class EvaluateJsonPathUsingCustomConfigurationTestCase {
         System.out.println(mobileNumber);
     }
 
+    @Test(priority = 3)
+    public void evaluateJsonPathUsingCustomConfigToSaveResultAsList() {
+
+        Configuration configuration =
+                Configuration
+                        .builder()
+                        .options(Option.ALWAYS_RETURN_LIST)
+                        .build();
+
+        String name = JsonPath.using(configuration).parse(sampleJson).read("$.[0].name");
+        System.out.println(name);
+    }
+
+    @Test(priority = 4)
+    public void evaluateJsonPathUsingCustomConfigWithRequirePropertiesOption() {
+
+        Configuration configuration =
+                Configuration
+                        .builder()
+                        .options(Option.ALWAYS_RETURN_LIST)
+                        .options(Option.REQUIRE_PROPERTIES)
+                        .build();
+
+        List<Integer> mobileNums = JsonPath.using(configuration).parse(sampleJson).read("$.[*].mobile");
+        System.out.println(mobileNums);
+    }
+
+    @Test(priority = 5)
+    public void evaluateJsonPathUsingCustomConfigWithReqPropAndSuppressWarnOption() {
+
+        Configuration configuration =
+                Configuration
+                        .builder()
+                        .options(Option.ALWAYS_RETURN_LIST)
+                        .options(Option.REQUIRE_PROPERTIES)
+                        .options(Option.SUPPRESS_EXCEPTIONS)
+                        .build();
+
+        List<Integer> mobileNums = JsonPath.using(configuration).parse(sampleJson).read("$.[*].mobile");
+        System.out.println(mobileNums);
+    }
 }
