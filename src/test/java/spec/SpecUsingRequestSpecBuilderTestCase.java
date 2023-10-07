bd4e5f6e68a0b37fd6b55ae47f3c51385ca1c554
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-10-07 18:23:46 +0530

    RequestSpecification Using RequestSpecBuilder

---
+++ src/test/java/spec/SpecUsingRequestSpecBuilderTestCase.java
@@ -0,0 +1,49 @@
+package spec;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class SpecUsingRequestSpecBuilderTestCase {
+
+    RequestSpecification requestSpecification;
+
+    @BeforeClass
+    public void setRequestSpecificationUsingRequestSpecBuilder(){
+
+        RequestSpecBuilder requestSpecBuilder = new RequestSpecBuilder();
+        requestSpecification = requestSpecBuilder
+                .setBaseUri("http://localhost:3000")
+                .setBasePath("/employees")
+                .build();
+    }
+
+    @Test(priority = 1)
+    public void getSingleEmployeeUsingApproach1(){
+
+        given()
+                .spec(requestSpecification)
+        .when()
+                .get("/1")
+        .then()
+                .statusCode(200)
+                .log()
+                .body();
+    }
+
+    @Test(priority = 2)
+    public void getSingleEmployeeUsingApproach2(){
+
+        given(requestSpecification)
+        .when()
+                .get("/1")
+        .then()
+                .statusCode(200)
+                .log()
+                .body();
+    }
+
+}
