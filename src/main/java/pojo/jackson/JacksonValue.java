e0aadf6342986a2ba473d653baf0bf1c9094cbed
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-10-08 09:30:26 +0530

    Serialization using Jackson Annotation (JsonValue)

---
+++ src/main/java/pojo/jackson/JacksonValue.java
@@ -0,0 +1,42 @@
+package pojo.jackson;
+
+import com.fasterxml.jackson.annotation.JsonValue;
+
+public class JacksonValue {
+
+    private String name;
+
+    private int id;
+
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @JsonValue
+    public String toString(){
+        return "["+this.id+" , "+ this.name+" , "+this.email+"]";
+    }
+
+}
---
+++ src/test/java/serialization/jackson_annotation/SerializationUsingJacksonValueTestCase.java
@@ -0,0 +1,23 @@
+package serialization.jackson_annotation;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.testng.annotations.Test;
+import pojo.jackson.JacksonValue;
+
+public class SerializationUsingJacksonValueTestCase {
+
+    @Test(priority = 1)
+    public void serializeUsingJacksonValue() throws JsonProcessingException {
+
+        JacksonValue jacksonValue = new JacksonValue();
+        jacksonValue.setId(1);
+        jacksonValue.setName("Ken");
+        jacksonValue.setEmail("ken@test.com");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String serializedJson = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(jacksonValue);
+        System.out.println(serializedJson);
+    }
+
+}
