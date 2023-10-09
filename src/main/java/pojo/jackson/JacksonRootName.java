29360ce0fac183c2775b0b6a500192c53c9f00b3
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-10-09 08:43:58 +0530

    Serialization using Jackson Annotation (JsonRootName)

---
+++ src/main/java/pojo/jackson/JacksonRootName.java
@@ -0,0 +1,40 @@
+package pojo.jackson;
+
+import com.fasterxml.jackson.annotation.JsonRawValue;
+import com.fasterxml.jackson.annotation.JsonRootName;
+
+@JsonRootName(value = "employee")
+public class JacksonRootName {
+
+    private String name;
+
+    private int id;
+
+    @JsonRawValue
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
---
+++ src/test/java/serialization/jackson_annotation/SerializationUsingJacksonRootNameTestCase.java
@@ -0,0 +1,25 @@
+package serialization.jackson_annotation;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.testng.annotations.Test;
+import pojo.jackson.JacksonRootName;
+
+public class SerializationUsingJacksonRootNameTestCase {
+
+    @Test(priority = 1)
+    public void serializeUsingJacksonRootName() throws JsonProcessingException {
+
+        JacksonRootName jacksonRootName = new JacksonRootName();
+        jacksonRootName.setId(1);
+        jacksonRootName.setName("Ken");
+        jacksonRootName.setEmail("ken@test.com");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.enable(SerializationFeature.WRAP_ROOT_VALUE);
+        String serializedJson = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(jacksonRootName);
+        System.out.println(serializedJson);
+    }
+
+}
