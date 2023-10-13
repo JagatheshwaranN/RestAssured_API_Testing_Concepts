68c0d1c78eec0159138f9f82e547f1e11c7145c9
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-10-13 10:19:46 +0530

    Post Method using Apache HttpClient

---
+++ src/main/java/_using_apache_httpclient/PostMethodUsingApacheHttpClientTest.java
@@ -0,0 +1,38 @@
+package _using_apache_httpclient;
+
+import org.apache.hc.client5.http.classic.HttpClient;
+import org.apache.hc.client5.http.classic.methods.HttpPost;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.ClassicHttpResponse;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
+import org.apache.hc.core5.http.io.entity.StringEntity;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+public class PostMethodUsingApacheHttpClientTest {
+
+    @Test(priority = 2)
+    public void postMethodUsingApacheHttpClient() throws IOException {
+        String payloadJson = """ 
+                                {
+                                   "name":"John",
+                                   "job":"Tester"
+                                  }
+                                """;
+        HttpClient httpClient = HttpClients.createDefault();
+        HttpPost request = new HttpPost("https://reqres.in/api/users");
+        request.setEntity(new StringEntity(payloadJson));
+        APIResponse response = httpClient.execute(request, httpResponse -> new APIResponse(EntityUtils.toString(httpResponse.getEntity()), httpResponse));
+        System.out.println(response.responseContainer.getCode());
+        System.out.println(response.responseContainer.getReasonPhrase());
+        System.out.println(response.responseBody);
+        System.out.println(Arrays.toString(response.responseContainer.getHeaders()));
+    }
+
+    record APIResponse(String responseBody, ClassicHttpResponse responseContainer) {
+    }
+
+}
+
