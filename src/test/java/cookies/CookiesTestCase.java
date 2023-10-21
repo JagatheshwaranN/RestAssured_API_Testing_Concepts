d4071cda23af2064b55fbc4faad107650358a521
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-10-21 08:13:13 +0530

    Cookies Scenarios Updated

--- src/test/java/cookies/CookiesTestCase.java
+++ src/test/java/cookies/CookiesTestCase.java
@@ -1,5 +1,7 @@
 package cookies;
 
+import io.restassured.http.Cookie;
+import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
 import org.testng.annotations.Test;
 
@@ -14,7 +16,63 @@ import static io.restassured.RestAssured.given;
 public class CookiesTestCase {
 
     @Test(priority = 1)
-    public void getCookieInfo(){
+    public void getCookiesInfoFromResponse(){
+
+        Response response =
+                given()
+                .when()
+                        .get("https://www.google.com/");
+
+        System.out.println(response.cookie("AEC"));
+
+        Map<String, String> cookies = response.getCookies();
+
+        for(Map.Entry<String, String> cookieEntry : cookies.entrySet()){
+            System.out.println(cookieEntry.getKey()+ " : "+ cookieEntry.getValue());
+        }
+    }
+
+    @Test(priority = 2)
+    public void getDetailedCookieInfoFromResponse(){
+
+        Response response =
+                given()
+                .when()
+                        .get("https://www.google.com/");
+
+        Cookie cookie = response.getDetailedCookie("AEC");
+
+        System.out.println("Cookie Name : " + cookie.getName());
+        if(cookie.hasValue()) {
+            System.out.println("Cookie Value      : " + cookie.getValue());
+        }
+        if(cookie.hasDomain()) {
+            System.out.println("Cookie Domain     : " + cookie.getDomain());
+        }
+        if(cookie.hasPath()) {
+            System.out.println("Cookie Path       : " + cookie.getPath());
+        }
+        if(cookie.hasComment()) {
+            System.out.println("Cookie Comment    : " + cookie.getComment());
+        }
+        if(cookie.hasExpiryDate()) {
+            System.out.println("Cookie ExpiryDate : " + cookie.getExpiryDate());
+        }
+        if(cookie.hasMaxAge()) {
+            System.out.println("Cookie MaxAge     : " + cookie.getMaxAge());
+        }
+        if(cookie.hasVersion()) {
+            System.out.println("Cookie Version    : " + cookie.getVersion());
+        }
+        if(cookie.hasSameSite()) {
+            System.out.println("Cookie SameSite   : " + cookie.getSameSite());
+        }
+        System.out.println("Cookie Is Http    : " + cookie.isHttpOnly());
+        System.out.println("Cookie Is Secured : " + cookie.isSecured());
+    }
+
+    @Test(priority = 3)
+    public void getCookieInfoFromValidatableResponse(){
 
        ValidatableResponse response =
                 given()
@@ -26,14 +84,14 @@ public class CookiesTestCase {
         System.out.println(response.extract().cookie("AEC"));
     }
 
-    @Test(priority = 2)
-    public void getCookiesInfo(){
+    @Test(priority = 4)
+    public void getCookiesInfoFromValidatableResponse(){
 
         ValidatableResponse response =
                 given()
-                        .when()
+                .when()
                         .get("https://www.google.com/")
-                        .then()
+                .then()
                         .statusCode(200);
 
         Map<String, String> cookies = response.extract().cookies();
@@ -49,4 +107,47 @@ public class CookiesTestCase {
             System.out.println(cookieEntry.getKey()+ " : "+cookieEntry.getValue());
         }
     }
+
+    @Test(priority = 5)
+    public void getDetailedCookieInfoFromValidatableResponse(){
+
+        ValidatableResponse response =
+                given()
+                .when()
+                        .get("https://www.google.com/")
+                .then()
+                        .statusCode(200);
+
+        Cookie cookie = response.extract().detailedCookie("ACE");
+
+        System.out.println("Cookie Name : " + cookie.getName());
+        if(cookie.hasValue()) {
+            System.out.println("Cookie Value      : " + cookie.getValue());
+        }
+        if(cookie.hasDomain()) {
+            System.out.println("Cookie Domain     : " + cookie.getDomain());
+        }
+        if(cookie.hasPath()) {
+            System.out.println("Cookie Path       : " + cookie.getPath());
+        }
+        if(cookie.hasComment()) {
+            System.out.println("Cookie Comment    : " + cookie.getComment());
+        }
+        if(cookie.hasExpiryDate()) {
+            System.out.println("Cookie ExpiryDate : " + cookie.getExpiryDate());
+        }
+        if(cookie.hasMaxAge()) {
+            System.out.println("Cookie MaxAge     : " + cookie.getMaxAge());
+        }
+        if(cookie.hasVersion()) {
+            System.out.println("Cookie Version    : " + cookie.getVersion());
+        }
+        if(cookie.hasSameSite()) {
+            System.out.println("Cookie SameSite   : " + cookie.getSameSite());
+        }
+        System.out.println("Cookie Is Http    : " + cookie.isHttpOnly());
+        System.out.println("Cookie Is Secured : " + cookie.isSecured());
+    }
+
+
 }
