cf95f223fddb35a9f6eccd9f747657d00413b4e7
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-10-21 11:17:39 +0530

    Set Cookies

---
+++ src/test/java/cookies/SetCookiesTestCase.java
@@ -0,0 +1,34 @@
+package cookies;
+
+import io.restassured.http.Cookie;
+import io.restassured.http.Cookies;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class SetCookiesTestCase {
+
+    @Test(priority = 1)
+    public void setCookieInRequest(){
+
+        given().
+                cookie("test", "123")
+        .when()
+                .get("https://www.google.com/");
+
+    }
+
+    @Test(priority = 2)
+    public void setCookiesInRequest(){
+
+        Cookie cookie1 = new Cookie.Builder("test", "123").setDomain("/").setSecured(true).build();
+        Cookie cookie2 = new Cookie.Builder("auto", "345").setDomain("/").setSecured(true).build();
+        Cookies cookies = new Cookies(cookie1, cookie2);
+        given().
+                cookies(cookies)
+        .when()
+                .get("https://www.google.com/");
+
+    }
+
+}
