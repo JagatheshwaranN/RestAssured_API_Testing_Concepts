19220c2bc0823c1b1d2a4ca9745d4b6af59d40aa
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-10-23 12:25:28 +0530

    Custom Parser Scenarios Added

--- src/test/java/response/parser/CustomParserTestCase.java
+++ src/test/java/response/parser/CustomParserTestCase.java
@@ -7,7 +7,7 @@ import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
 
-public class RegisterAndUnRegisterParserTestCase {
+public class CustomParserTestCase {
 
     @Test(priority = 1)
     public void registerAndUnRegisterParser(){
@@ -17,12 +17,24 @@ public class RegisterAndUnRegisterParserTestCase {
                 given()
                         .when()
                         .get("https://wttr.in/Location")
-                        .then()
-                        .using()
-                        .defaultParser(Parser.JSON);
+                        .then();
 
         System.out.println("Response : " +  response.extract().asPrettyString());
         RestAssured.unregisterParser("text/html");
+    }
+
+    @Test(priority = 2)
+    public void customParser(){
+
+        ValidatableResponse response =
+                given()
+                .when()
+                        .get("https://wttr.in/Location")
+                .then()
+                        .using()
+                        .parser("text/html", Parser.HTML);
+
+        System.out.println("Response : " +  response.extract().asPrettyString());
     }
 
 }
