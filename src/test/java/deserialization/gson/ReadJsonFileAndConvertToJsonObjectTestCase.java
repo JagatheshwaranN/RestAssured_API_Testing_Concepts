3b81ec08381ba3a085d1a3db8815631af77c1fea
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-10-27 08:15:59 +0530

    Read Json File / String and Convert it to JsonObject, and read the data

---
+++ src/test/java/deserialization/gson/ReadJsonFileAndConvertToJsonObjectTestCase.java
@@ -0,0 +1,45 @@
+package deserialization.gson;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import org.testng.annotations.Test;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
+public class ReadJsonFileAndConvertToJsonObjectTestCase {
+
+    @Test(priority = 1)
+    public void readJsonFileAndConvertToJsonObject() {
+
+        Gson gson = new Gson();
+        String jsonFile = "src/test/resources/users.json";
+        JsonObject jsonObject = getJsonObjectFromJson(jsonFile, gson);
+        JsonObject baseNode = jsonObject.getAsJsonObject("john");
+        String firstName = baseNode.get("firstname").getAsString();
+        JsonArray array = baseNode.getAsJsonArray("skills");
+        String street = baseNode.getAsJsonObject("address").get("street").getAsString();
+        System.out.println(firstName);
+        System.out.println(array);
+        System.out.println(street);
+    }
+
+    private JsonObject getJsonObjectFromJson(String file, Gson gson) {
+
+        JsonElement jsonElement;
+        JsonObject jsonObject;
+        try (Reader reader = new FileReader(file)) {
+            jsonElement = gson.fromJson(reader, JsonElement.class);
+            // If needed, we can use below conversion.
+            // String jsonString = gson.toJson(jsonElement);
+            jsonObject = jsonElement.getAsJsonObject();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return jsonObject;
+    }
+
+}
---
+++ src/test/java/deserialization/gson/ReadJsonStringAndConvertToJsonObjectTestCase.java
@@ -0,0 +1,48 @@
+package deserialization.gson;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.testng.annotations.Test;
+
+public class ReadJsonStringAndConvertToJsonObjectTestCase {
+
+    @Test(priority = 1)
+    public void readJsonStringAndConvertToJsonObject() {
+
+        String jsonString = """
+                {
+                   "john": {
+                     "firstname": "John",
+                     "lastname": "Doe",
+                     "age": 30,
+                     "skills": [
+                       "Java",
+                       "Python",
+                       "JavaScript"
+                     ],
+                     "address": {
+                       "street": "1234 Elm Street",
+                       "city": "Any-town",
+                       "state": "CA",
+                       "zipcode": "12345"
+                     }
+                   }
+                 }
+                """;
+
+        JsonObject jsonObject = getJsonObjectFromJson(jsonString);
+        JsonObject baseNode = jsonObject.getAsJsonObject("john");
+        String firstName = baseNode.get("firstname").getAsString();
+        JsonArray array = baseNode.getAsJsonArray("skills");
+        String street = baseNode.getAsJsonObject("address").get("street").getAsString();
+        System.out.println(firstName);
+        System.out.println(array);
+        System.out.println(street);
+    }
+
+    private JsonObject getJsonObjectFromJson(String json) {
+        return JsonParser.parseString(json).getAsJsonObject();
+    }
+
+}
