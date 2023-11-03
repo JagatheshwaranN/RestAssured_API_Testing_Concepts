931376fff49fa43988140e992bac0c7ab93e5542
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-11-03 08:09:07 +0530

    Mocking - WireMock - Get Api Mocking

--- src/test/java/mock/mocky_io/MockGetAPITestCase.java
+++ src/test/java/mock/mocky_io/MockGetAPITestCase.java
@@ -1,4 +1,4 @@
-package mock;
+package mock.mocky_io;
 
 import org.testng.annotations.Test;
 
---
+++ src/test/java/mock/wiremock/MockGetAPITestCase.java
@@ -0,0 +1,44 @@
+package mock.wiremock;
+
+import io.restassured.response.ValidatableResponse;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+/**
+ * @restapi
+ * @get
+ * @mock
+ */
+public class MockGetAPITestCase {
+
+    @Test(priority = 1)
+    public void mockGetApiResponseTest(){
+
+        given()
+        .when()
+                .get("http://localhost:8080/user/1")
+        .then()
+                .statusCode(200)
+                .log()
+                .all();
+    }
+
+    @Test(priority = 2)
+    public void mockGetApiResponseHeaderTest(){
+        ValidatableResponse response =
+        given()
+        .when()
+                .get("http://localhost:8080/user/2")
+        .then()
+                .statusCode(200)
+                .log()
+                .all();
+
+        String contentType = response.extract().header("Content-Type");
+        System.out.println("Response Content Type : " + contentType);
+        Assert.assertEquals(contentType, "text/plain");
+    }
+
+}
