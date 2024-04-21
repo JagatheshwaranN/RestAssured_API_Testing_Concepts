1b878bb5b60474ba6e10d6651d15768b8e8046ed
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2024-04-21 15:46:57 +0530

    Rest Assured Knowledge Share - Spec and Proxy Notes added

--- src/test/java/proxy/ProxySettingTestCase.java
+++ src/test/java/proxy/ProxySettingTestCase.java
@@ -1,12 +1,13 @@
 package proxy;
 
 import io.restassured.RestAssured;
+import io.restassured.specification.ProxySpecification;
 import org.testng.annotations.Test;
 
 public class ProxySettingTestCase {
 
     @Test(priority = 1)
-    public void proxySetting(){
+    public void proxySettingType1(){
 
         RestAssured
                 .given()
@@ -18,4 +19,19 @@ public class ProxySettingTestCase {
                         .log()
                         .body();
     }
+
+    @Test(priority = 2)
+    public void proxySettingType2(){
+        ProxySpecification proxySpec = new ProxySpecification("localhost", 8080, "http");
+        RestAssured
+                .given()
+                    .proxy(proxySpec)
+                .when()
+                    .get("http://localhost:3000/students")
+                .then()
+                    .statusCode(200)
+                    .log()
+                    .body();
+    }
+
 }
